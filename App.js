9fd6f6b67d47e0b3c752ce98888d5522533a5c60
lenguyenkhoi21 <lenguyenkhoi21@gmail.com>
2022-03-16 22:28:00 +0700

    fix env

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<Text>
-				Open up App.js to start working on your app fasfsdfasfasdfasdfas
-				asdfasfasfasdfasdfasdfasdfasd fasfasfasdfasdfa
-				asfasadfasdfasdfasdfasdfasdff! fasdjfaslfjaslfjasdlfjdaslfjasdlfjsaldfja
-				dfasdfsdf asfdasdf asdf slfjasldfjsaldfjsladfjaslfjasd fasdfasfasdfasdfasdfsafasfasdf
 			</Text>
 			<StatusBar style='auto' />
 		</View>
